b8c80fbbe652b8412a148184c9ae2df91beb1c1c
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-16 23:32:35 +0600

    set gooogle singin

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBHVkT2E_oO0i_WPA_okLNw1xTkxWjATk0',
+  authDomain: 'auth-recap-b3ead.firebaseapp.com',
+  projectId: 'auth-recap-b3ead',
+  storageBucket: 'auth-recap-b3ead.appspot.com',
+  messagingSenderId: '836242619937',
+  appId: '1:836242619937:web:9f825d91f7416ad885ed60',
+};
+
+export default firebaseConfig;
